bec3debe3b3dd077de6748314272ee86589e4742
strungar <nikita@yefimov.pp.ua>
2025-10-25 20:00:15 +0300

    add github prepare action

---
+++ builder-image/build.bat
@@ -0,0 +1,21 @@
+
+set file=%1
+if %file%.==. goto noparam
+
+if exist ".env\" (
+python -m virtualenv .env
+call .env/Scripts/activate.bat
+@echo on
+python -m pip install -r requirements.txt
+) else (
+call .env/Scripts/activate.bat
+@echo on
+)
+pyinstaller --noconfirm %file%
+goto end
+
+:noparam
+echo filename not set
+echo usage docker run -v .:/app/src --workdir /app/src -it pywininstaller main.py
+
+:end
